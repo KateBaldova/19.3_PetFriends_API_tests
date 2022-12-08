ffe88848dfd1018850173ba669b6b7f9a8960b33
Ekaterina Baldova <kim2005_86@mail.ru>
2022-12-08 21:23:15 +0300

    Some changes have been added (xfail, fixture)

--- tests/test_delete_pets.py
+++ tests/test_delete_pets.py
@@ -1,41 +1,45 @@
 from api import PetFriends
-from settings import valid_email, valid_password
-import os
+from conftest import ket_api_key
+from datatest import TestFunc
+import pytest
 
+tf = TestFunc()
+kak = ket_api_key
 pf = PetFriends()
 
 def test_delete_pet_with_valid_data():
     '''Проверяем возможность удаления своего питомца'''
-    _, auth_key = pf.get_api_key(valid_email, valid_password)
-    _, my_pets = pf.get_list_of_pets(auth_key, 'my_pets')
+
+    _, my_pets = pf.get_list_of_pets(pytest.key, 'my_pets')
 
     if len(my_pets['pets']) == 0:
-        pf.add_new_pet(auth_key, "Суперкот", "кот", "3", "images/photo_2022-10-11_12-34-21.jpg")
-        _, my_pets = pf.get_list_of_pets(auth_key, "my_pets")
+        pf.add_new_pet(pytest.key, "Суперкот", "кот", "3", "images/photo_2022-10-11_12-34-21.jpg")
+        _, my_pets = pf.get_list_of_pets(pytest.key, "my_pets")
 
         # Берём id первого питомца из списка и отправляем запрос на удаление
     pet_id = my_pets['pets'][0]['id']
-    status, _ = pf.delete_pet_petid(auth_key, pet_id)
+    pytest.status, _ = pf.delete_pet_petid(pytest.key, pet_id)
 
     # Ещё раз запрашиваем список своих питомцев
-    _, my_pets = pf.get_list_of_pets(auth_key, "my_pets")
+    _, my_pets = pf.get_list_of_pets(pytest.key, "my_pets")
 
     # Проверяем что статус ответа равен 200 и в списке питомцев нет id удалённого питомца
-    assert status == 200
+    assert pytest.status == 200
     assert pet_id not in my_pets.values()
 
+@pytest.mark.fail
 def test_delete_pet_another_user():
     '''Проверяем возможность удаления питомца другого пользователя'''
-    _, auth_key = pf.get_api_key(valid_email, valid_password)
-    _, all_pets = pf.get_list_of_pets(auth_key, '')
+
+    _, all_pets = pf.get_list_of_pets(pytest.key, '')
 
     # Берём id первого питомца из общего списка и отправляем запрос на удаление
     pet_id = all_pets['pets'][0]['id']
-    status, _ = pf.delete_pet_petid(auth_key, pet_id)
+    pytest.status, _ = pf.delete_pet_petid(pytest.key, pet_id)
 
     # Ещё раз запрашиваем список всех питомцев
-    _, all_pets = pf.get_list_of_pets(auth_key, '')
+    _, all_pets = pf.get_list_of_pets(pytest.key, '')
 
     # Проверяем что статус ответа равен 403 и в списке питомцев есть id питомца, которого пытались удалить
-    assert status == 403 #в настоящее время тест отрабатывает неверно - это баг, питомец удаляется, статус код 200
+    assert pytest.status == 403 #в настоящее время тест отрабатывает неверно - это баг, питомец удаляется, статус код 200
     assert pet_id in all_pets.values() #в настоящее время тест отрабатывает неверно - это баг, питомец удаляется, его id нет в списке всех питомцев
--- tests/test_update_pet_info.py
+++ tests/test_update_pet_info.py
@@ -1,39 +1,41 @@
-from api import PetFriends, generate_string
-from settings import valid_email, valid_password
-import os
+from api import PetFriends
+from conftest import ket_api_key
+from datatest import TestFunc
+import pytest
 
+tf = TestFunc()
+kak = ket_api_key
 pf = PetFriends()
 
-def test_successful_update_self_pet_info(name='Ася', animal_type='русская голубая', age=13):
+def test_successful_update_self_pet_info(name='Асия', animal_type='русская голубая', age=13):
     """Проверяем возможность обновления информации о питомце"""
 
     # Получаем ключ auth_key и список своих питомцев
-    _, auth_key = pf.get_api_key(valid_email, valid_password)
-    _, my_pets = pf.get_list_of_pets(auth_key, "my_pets")
+    _, my_pets = pf.get_list_of_pets(pytest.key, "my_pets")
 
     # Если список не пустой, то пробуем обновить его имя, тип и возраст
     if len(my_pets['pets']) > 0:
-        status, result = pf.update_pet_info(auth_key, my_pets['pets'][0]['id'], name, animal_type, age)
+        pytest.status, result = pf.update_pet_info(pytest.key, my_pets['pets'][0]['id'], name, animal_type, age)
 
         # Проверяем что статус ответа = 200 и имя питомца соответствует заданному
-        assert status == 200
+        assert pytest.status == 200
         assert result['name'] == name
     else:
         # если спиок питомцев пустой, то выкидываем исключение с текстом об отсутствии своих питомцев
         raise Exception("There is no my pets")
 
+@pytest.mark.xfail
 def test_update_pet_info_of_another_user(name='Рубикон', animal_type='корги', age=3):
     """Проверяем возможность обновления информации о питомце другого юзера"""
 
     # Получаем ключ auth_key и список всех питомцев
-    _, auth_key = pf.get_api_key(valid_email, valid_password)
-    _, all_pets = pf.get_list_of_pets(auth_key, "")
+    _, all_pets = pf.get_list_of_pets(pytest.key, "")
     old_name = all_pets['pets'][0]['name']
 
-    status, result = pf.update_pet_info(auth_key, all_pets['pets'][0]['id'], name, animal_type, age)
-    _, all_pets_new = pf.get_list_of_pets(auth_key, "")
+    pytest.status, result = pf.update_pet_info(pytest.key, all_pets['pets'][0]['id'], name, animal_type, age)
+    _, all_pets_new = pf.get_list_of_pets(pytest.key, "")
 
     #Проверяем что статус ответа = 500 и имя питомца не соответствует заданному
     assert all_pets['pets'][0]['name'] == old_name
     assert name not in all_pets_new.values()
-    assert status == 500 #в данный момент статус код = 200, хотя изменения данных чужого питомца не происходит. Баг?
+    assert pytest.status == 500 #в данный момент статус код = 200, хотя изменения данных чужого питомца не происходит. Баг?
